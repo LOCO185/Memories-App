0875d6fa96c8daadea3387dbbe9230d9cff38a11
LOCO185 <jloco151@gmail.com>
2022-02-04 16:15:51 +0200

    this commit will add .env variables + constants

---
+++ client/src/constants/actionTypes.js
@@ -0,0 +1,5 @@
+export const CREATE = 'CREATE';
+export const DELETE = 'DELETE';
+export const UPDATE = 'UPDATE';
+export const FETCH_ALL = 'FETCH_ALL';
+export const LIKE = 'LIKE';
--- server/index.js
+++ server/index.js
@@ -2,24 +2,27 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+import dotenv from "dotenv";
 
-import postRoutes from './routes/posts.js'
+import postRoutes from "./routes/posts.js";
 
 const app = express();
-
+dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 // http://localhost:5000/posts
-app.use('/posts', postRoutes);
-const CONNECTION_URL =
-  "mongodb+srv://jawad-memories:jawadmemories123@cluster0.zdccr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+app.use("/posts", postRoutes);
+
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() =>
     app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
   )
